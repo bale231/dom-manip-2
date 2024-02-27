b7488294ca88c834221864c07f6a7b39d2ccc5c8
Luigi Balestrucci <card3001@outlook.it>
2024-02-27 18:51:21 +0100

    Change title html

--- script.js
+++ script.js
@@ -2,13 +2,14 @@ const inputName = document.getElementById('firstName');
 const inputLastName = document.getElementById('lastName');
 const inputAge = document.getElementById('age');
 const form = document.querySelector('form');
+const div = document.querySelector('.divnot');
+
 
 const person = {
     firstName: inputName.value,
     lastName: inputLastName.value,
     age: inputAge.value,
-}
+};
 
 jsonPerson = JSON.stringify(person);
-
 form.setAttribute("data-person", jsonPerson)
